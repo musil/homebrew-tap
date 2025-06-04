24db664c268cbe2b825aa8147c475b427ea545fc
Stanislav Musil <musil@sm-net.cz>
2025-06-04 21:56:39 +0200

    feat: v0.6.3

--- Formula/smu.rb
+++ Formula/smu.rb
@@ -6,7 +6,7 @@ class Smu < Formula
   
     on_macos do
         if Hardware::CPU.arm?
-          url "https://smu.dc5.eu/dist/smu_v0.6.2_darwin_arm64.tar.gz"
+          url "https://smu.dc5.eu/dist/smu_v0.6.3_darwin_arm64.tar.gz"
           sha256 "db7b188a9c0eb28d47b4881dcaf6b6580ebd2984c3d7aedfe07250ba1939cbbe"
     
           def install
@@ -17,7 +17,7 @@ class Smu < Formula
     
       on_linux do
         if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-          url "https://smu.dc5.eu/dist/smu_v0.6.2_linux_arm64.tar.gz"
+          url "https://smu.dc5.eu/dist/smu_v0.6.3_linux_arm64.tar.gz"
           sha256 "12620882eefa6dc893faf9cdbd2bab3fd7a837b25cf3ee520ec2a6c73ca2e5ef"
     
           def install
@@ -25,7 +25,7 @@ class Smu < Formula
           end
         end
         if Hardware::CPU.intel?
-          url "https://smu.dc5.eu/dist/smu_v0.6.2_linux_amd64.tar.gz"
+          url "https://smu.dc5.eu/dist/smu_v0.6.3_linux_amd64.tar.gz"
           sha256 "ea53be5717c0cbe0e58ea71389173cbdcd99b61a6cd5eb50426751578b37320c"
     
           def install
